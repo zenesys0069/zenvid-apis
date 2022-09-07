c926061d504c41b0befd325c7cd3dd7b93bb4d2f
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2022-09-08 00:53:19 +0530

    add mongodb

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routers/index.js'
+import mongodbInit from './configs/mongodb.js'
 
 // express app
 const app = express()
@@ -14,11 +15,22 @@ app.use(cors())
 app.use(express.json())
 
 // server static files
+// TODO
 
 // routes
 Object.entries(routes).map(([name, router]) => {
   app.use(`${API_PREFIX}/${name}`, router)
 })
+
+// connecting app to mongodb database
+mongodbInit()
+  .then((res) => console.log('Connected to local mongodb database'))
+  .catch((err) =>
+    console.log(
+      'There was an error while connecting to the mongodb database',
+      err
+    )
+  )
 
 // root route
 app.get('/', (req, res) => {
---
+++ configs/mongodb.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+// initialize the mongodb local connection
+const mongodbInit = () => {
+  // create a new promise that return the status
+  return new Promise((resolve, reject) => {
+    mongoose
+      .connect('mongodb://127.0.0.1:27017/shopexe')
+      .then((res) => {
+        // successfully connected so resolve the promise
+        resolve(res)
+      })
+      .catch((err) => {
+        // there was some error while connecting to the mongodb reject the promise
+        reject(err)
+      })
+  })
+}
+
+export default mongodbInit
