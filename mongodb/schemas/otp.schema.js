5bad5d70f6a1c548afec2ef123b3bcf1801fc50d
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2022-09-27 15:48:50 +0530

    add timestamps

--- mongodb/schemas/otp.schema.js
+++ mongodb/schemas/otp.schema.js
@@ -2,18 +2,23 @@ import mongoose from 'mongoose'
 
 const { Schema } = mongoose
 
-const otpSchema = new Schema({
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-    required: true,
+const otpSchema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+    otp: {
+      type: Number,
+      trim: true,
+      required: true,
+    },
   },
-  otp: {
-    type: Number,
-    trim: true,
-    required: true,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 export default otpSchema
