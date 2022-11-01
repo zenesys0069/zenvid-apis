5c372bfe55fce21c35146bf484fcaafffe447fe0
AKSHAY KUMAR SINGH <akshaymemail@gmail.com>
2022-11-01 16:07:56 +0530

    refactor info controller

--- api/v1/controllers/info.controllers.mjs
+++ api/v1/controllers/info.controllers.mjs
@@ -1,53 +1,41 @@
 import os from 'os'
+import helpers from '../../../helpers/index.mjs'
 export const version = (req, res) => {
-  res.status(200).json({
-    status: true,
-    message: 'Successfully completed!',
-    result: {
-      version: 'v1.0.0',
-    },
-  })
+  const result = {
+    version: 'v1.0.0',
+  }
+  helpers.common.successHandler(res, null, null, result)
 }
 
 export const software = (req, res) => {
-  res.status(200).send({
-    status: true,
-    message: 'Successfully completed!',
-    result: {
-      server: 'Express 4.x.x',
-      environment: 'Node LTS 16.x.x',
-      database: 'MongoDB 6.x.X',
-    },
-  })
+  const result = {
+    server: 'Express 4.x.x',
+    environment: 'Node LTS 16.x.x',
+    database: 'MongoDB 6.x.X',
+  }
+  helpers.common.successHandler(res, null, null, result)
 }
 
 export const hardware = (req, res) => {
-  res.status(200).send({
-    status: true,
-    message: 'Successfully completed!',
-    result: {
-      os: {
-        type: os.type(),
-        release: os.release(),
-        platform: os.platform(),
-      },
-      cpu: os.cpus()[0].model,
+  const result = {
+    os: {
+      type: os.type(),
+      release: os.release(),
+      platform: os.platform(),
     },
-  })
+    cpu: os.cpus()[0].model,
+  }
+  helpers.common.successHandler(res, null, null, result)
 }
 
 export const status = (req, res) => {
   const seconds = Math.floor(process.uptime())
-
-  res.status(200).send({
-    status: true,
-    message: 'Successfully completed!',
-    result: {
-      protocol: req.protocol,
-      host: req.get('host'),
-      port: global.listener.address().port,
-      family: global.listener.address().family,
-      uptime: `${seconds}s`,
-    },
-  })
+  const result = {
+    protocol: req.protocol,
+    host: req.get('host'),
+    port: global.listener.address().port,
+    family: global.listener.address().family,
+    uptime: `${seconds}s`,
+  }
+  helpers.common.successHandler(res, null, null, result)
 }
